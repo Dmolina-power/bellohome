cd7a3bf4e0b9fe7bbc93963e67f27e3b4b210653
daniel molina <bigdpower@icloud.com>
2020-10-17 16:50:49 -0700

    added styles fixed navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { auth } from "../../fire";
 import UserInfo from "../UserInfo/index";
+import logo from "../../images/theme2.jpg";
 import "./style.css";
+
 
 const Navbar = ({ handleLogout }) => {
   
@@ -10,7 +12,7 @@ const Navbar = ({ handleLogout }) => {
     <section className="home">
       <nav class="navbar navbar-expand-lg navbar-light bg-light position-sticky">
         <a class="navbar-brand" href="/home">
-          LOGO
+        <img src={logo} width="160" height="60" alt="" />
         </a>
         <button
           class="navbar-toggler"
@@ -27,40 +29,37 @@ const Navbar = ({ handleLogout }) => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="/studio">
-                My Studio<span class="sr-only">(current)</span>
+              <a class="nav-link" href="/services">
+                Services<span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/feed">
-                Art Feed
+              <a class="nav-link" href="/payment">
+                Payment Options
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/Inbox">
-                Inbox
+              <a class="nav-link" href="/promotions">
+                Promotions
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/cart">
-                Cart
+              <a class="nav-link" href="/why">
+               Why Choose Us
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/team">
+               Our Team
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">
+               Contact Us
               </a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search for Artist"
-              aria-label="Search"
-            ></input>
-            <button
-              className="search"
-              type="submit"
-            >
-              Search
-            </button>
-          </form>
+          
           <UserInfo />
 
         <button className="exit"
